8a445269794b8a38d726c7fa4878e2e821fcaa49
nadia <nadialuana31@gmail.com>
2024-05-23 17:12:12 -0300

    ponto h do joystick

---
+++ joystick.h
@@ -0,0 +1,28 @@
+#ifndef _JOYSTICK_H_
+#define _JOYSTICK_H_
+
+// ESTRUTURA ==========================================
+
+struct joystick {
+	char right;
+	char left;
+	char up;
+	char down;
+}
+
+// FUNCOES ============================================
+
+struct joystick *joystick_create();
+
+void joystick_destroy(struct joystick *element);
+
+void joystick_right(struct joystick *element);
+
+void joystick_left(struct joystick *element);
+
+void joystick_up(struct joystick *element);
+
+void joystick_down(struct joystick *element);
+
+
+#endif
